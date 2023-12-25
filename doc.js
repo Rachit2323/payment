e7995afc4b1e1f31884e28546c30ea965b91ba24
Rachit2323 <76684704+Rachit2323@users.noreply.github.com>
2023-12-25 13:13:32 +0530

    done

--- doc.js
+++ doc.js
@@ -75,7 +75,7 @@ exports.paymentVerification = async (req, res) => {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
 
     console.log(
-      "rin",
+      "run",
       razorpay_order_id,
       razorpay_payment_id,
       razorpay_signature
@@ -99,7 +99,6 @@ exports.paymentVerification = async (req, res) => {
   } catch (error) {
     console.error("Error processing payment verification:", error);
 
-    // Send JSON response in case of an error
     return res.status(400).json({
       success: false,
       error: "Authentication failed",
@@ -108,70 +107,11 @@ exports.paymentVerification = async (req, res) => {
 };
 
 
-// exports.paymentVerification = async (req, res) => {
-//   console.log("wor");
-//   const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
-//     req.body;
-
-//   const body = razorpay_order_id + "|" + razorpay_payment_id;
-
-//   const expectedSignature = crypto
-//     .createHmac("sha256", process.env.RAZORPAY_API_SECRET) // Corrected the typo here
-//     .update(body)
-//     .digest("hex");
-
-//   const isAuthentic = expectedSignature === razorpay_signature;
-//   console.log(isAuthentic, expectedSignature, razorpay_signature);
-//   if (isAuthentic) {
-//     try {
-//       // Database logic comes here
-//       console.log(
-//         "rin",
-//         razorpay_order_id,
-//         razorpay_payment_id,
-//         razorpay_signature
-//       );
-//       await Payment.create({
-//         razorpay_order_id,
-//         razorpay_payment_id,
-//         razorpay_signature,
-//       });
-
-//       console.log("Payment data stored in the database");
-
-//       res.redirect(
-//         `http://localhost:3000/paymentsuccess?reference=${razorpay_payment_id}`
-//       );
-//     } catch (error) {
-//       console.error("Error storing payment data in the database:", error);
-//       res.status(500).json({ success: false, error: "Internal Server Error" });
-//     }
-//   }
-//   // if (isAuthentic) {
-//   //   console.log('check');
-//   //   // Database logic comes here
-//   //   await Payment.create({
-//   //     razorpay_order_id,
-//   //     razorpay_payment_id,
-//   //     razorpay_signature,
-//   //   });
-
-//   //   res.redirect(`http://localhost:3000/paymentsuccess?reference=${razorpay_payment_id}`);
-//   // }
-//   else {
-//     res.status(400).json({
-//       success: false,
-//     });
-//   }
-// };
 
 exports.paymentcheck = async (req, res) => {
   try {
-    // const instance = new Razorpay({
-    //   key_id: process.env.RAZORPAY_API_KEY,
-    //   key_secret: process.env.RAZORPAY_API_SECRET,
-    // });
-    console.log('check')
+ 
+
     var instance = new Razorpay({
       key_id: process.env.RAZORPAY_API_KEY,
       key_secret: process.env.RAZORPAY_API_SECRET,
@@ -179,10 +119,10 @@ exports.paymentcheck = async (req, res) => {
     const options = {
       amount: Number(req.body.amount * 100),
       currency: "INR",
-      // receipt: shortid.generate(),
+
     };
     const order = await instance.orders.create(options);
-    // console.log('rde',order);
+
     res.status(200).json({
       success: true,
       order,
